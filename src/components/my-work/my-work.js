389a798e1aa7bd5b2b088c33ff65f6bc39c7e4e4
Timebox Designs <timebox-designs@gmail.com>
2022-02-02 17:06:12 -0700

    fix: restored original content

--- src/components/my-work/my-work.js
+++ src/components/my-work/my-work.js
@@ -1,9 +1,7 @@
 import React, {Fragment} from "react";
-import classNames from "classnames";
+import clsx from "clsx";
 import {Col, Grid, Row} from "react-flexbox-grid";
 import {Element} from "react-scroll";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faQuoteLeft} from "@fortawesome/free-solid-svg-icons";
 import styles from "./my-work.module.css";
 
 import bistro from "../../img/grammable_bistro_showreel.jpg";
@@ -19,226 +17,208 @@ import n from "../../img/n.png";
 
 const GRAMMABLE_BISTRO = "https://dissolve.com/showreels/grammable";
 const VERTICAL_READY =
-  "http://ideas.dissolve.com/tips/get-vertical-ready-with-dissolve";
+    "http://ideas.dissolve.com/tips/get-vertical-ready-with-dissolve";
 const MMUUULLTTIIPPLLY =
-  "http://ideas.dissolve.com/tips/achieving-the-mmuuullttiipplly-effect-in-adobe-after-effects";
+    "http://ideas.dissolve.com/tips/achieving-the-mmuuullttiipplly-effect-in-adobe-after-effects";
 
 const QUEENS_PARK =
-  "https://tourismnewwestminster.com/blog/spring-break-guide-new-west/";
+    "https://tourismnewwestminster.com/blog/spring-break-guide-new-west/";
 const CABARET =
-  "https://tourismnewwestminster.com/blog/cabaret-cocktails-new-west/";
+    "https://tourismnewwestminster.com/blog/cabaret-cocktails-new-west/";
 const COMMUNITY =
-  "https://tourismnewwestminster.com/blog/river-market-craft-cafe/";
+    "https://tourismnewwestminster.com/blog/river-market-craft-cafe/";
 
 const MyWork = () => {
   return (
-    <Fragment>
-      <section className={classNames(styles.container, styles.inverse)}>
-        <Element name="my-work">
+      <Fragment>
+        <section className={clsx(styles.container, styles.inverse)}>
+          <Element name="my-work">
+            <Grid className={styles.grid} fluid>
+              <Row style={{marginTop: "2rem", marginBottom: "1rem"}}>
+                <Col sm={12}>
+                  <h5>MY WORK</h5>
+                </Col>
+              </Row>
+
+              <Row>
+                <Col sm={8}>
+                  <a href={GRAMMABLE_BISTRO}>
+                    <img
+                        className={styles.img}
+                        src={bistro}
+                        alt="The Grammable Bistro"
+                    />
+                  </a>
+                </Col>
+                <Col sm={3}>
+                  <a className={styles.caption} href={GRAMMABLE_BISTRO}>
+                    <p style={{paddingLeft: "2rem"}}>
+                      Introducing North America's first authentic, handcrafted,
+                      gastro-photogenical experience.
+                    </p>
+                  </a>
+                </Col>
+              </Row>
+            </Grid>
+          </Element>
+        </section>
+
+        <section>
           <Grid className={styles.grid} fluid>
+            <Row style={{marginTop: "4rem", marginBottom: "4rem"}}>
+              <Col sm={8}>
+                <div className={styles.copy}>
+                  <p>
+                    I understand the importance and hard work that goes into
+                    creating an authentic brand. Check out a few of the brands
+                    below which have trusted me with developing and maintaining
+                    their online voice.
+                  </p>
+                </div>
+              </Col>
+            </Row>
+
             <Row style={{marginTop: "2rem", marginBottom: "1rem"}}>
               <Col sm={12}>
-                <h5>MY WORK</h5>
+                <h5>DISSOLVE</h5>
+              </Col>
+            </Row>
+
+            <Row style={{marginBottom: "2rem"}}>
+              <Col sm={4}>
+                <a href={GRAMMABLE_BISTRO}>
+                  <img
+                      className={styles.img}
+                      src={surprised}
+                      alt="The Grammable Bistro"
+                  />
+                </a>
+                <h6>The Grammable Bistro - Inedible Food, Stunning Pictures.</h6>
+              </Col>
+              <Col sm={4}>
+                <a href={VERTICAL_READY}>
+                  <img
+                      className={styles.img}
+                      src={vertical}
+                      alt="Get Vertical Ready with Dissolve"
+                  />
+                </a>
+                <h6>Get Vertical Ready with Dissolve</h6>
+              </Col>
+              <Col sm={4}>
+                <a href={MMUUULLTTIIPPLLY}>
+                  <img
+                      className={styles.img}
+                      src={mmuuullttiipplly}
+                      alt="Achieving the MMUUULLTTIIPPLLY effect"
+                  />
+                </a>
+                <h6>Achieving the MMUUULLTTIIPPLLY effect</h6>
               </Col>
             </Row>
 
             <Row>
               <Col sm={8}>
-                <a href={GRAMMABLE_BISTRO}>
-                  <img
-                    className={styles.img}
-                    src={bistro}
-                    alt="The Grammable Bistro"
-                  />
-                </a>
-              </Col>
-              <Col sm={3}>
-                <a className={styles.caption} href={GRAMMABLE_BISTRO}>
-                  <p style={{paddingLeft: "2rem"}}>
-                    Introducing North America's first authentic, handcrafted,
-                    gastro-photogenical experience.
-                  </p>
-                </a>
+                <p>
+                  Wrote engaging, brand friendly, content and scripts for digital
+                  and print ads, media outreach, and social media, increasing
+                  brand awareness, click through, and sales.
+                </p>
               </Col>
             </Row>
           </Grid>
-        </Element>
-      </section>
+        </section>
 
-      <section>
-        <Grid className={styles.grid} fluid>
-          <Row style={{marginTop: "4rem", marginBottom: "4rem"}}>
-            <Col sm={8}>
-              <div className={styles.copy}>
-                <div style={{fontSize: "4rem", marginBottom: "1rem"}}>
-                  <FontAwesomeIcon icon={faQuoteLeft} />
-                </div>
+        <section className={styles.inverse}>
+          <Grid className={styles.grid} fluid>
+            <Row style={{marginTop: "2rem", marginBottom: "1rem"}}>
+              <Col sm={12}>
+                <h5>TOURISM NEW WESTMINSTER</h5>
+              </Col>
+            </Row>
+
+            <Row style={{marginBottom: "2rem"}}>
+              <Col sm={4}>
+                <a href={QUEENS_PARK}>
+                  <img
+                      className={styles.img}
+                      src={park}
+                      alt="Spring Break For the Whole Family"
+                  />
+                </a>
+                <h6>Spring Break For the Whole Family;</h6>
+              </Col>
+              <Col sm={4}>
+                <a href={CABARET}>
+                  <img
+                      className={styles.img}
+                      src={cabaret}
+                      alt="Cabaret & Tasty Cocktails"
+                  />
+                </a>
+                <h6>Cabaret & Tasty Cocktails;</h6>
+              </Col>
+              <Col sm={4}>
+                <a href={COMMUNITY}>
+                  <img
+                      className={styles.img}
+                      src={community}
+                      alt="Art & Community Spaces at the Craft Cafe"
+                  />
+                </a>
+                <h6>Art & Community Spaces at the Craft Cafe</h6>
+              </Col>
+            </Row>
+
+            <Row>
+              <Col sm={8}>
                 <p>
-                  Hey, three fuzzy images and a yelp review! Someone quick, hand
-                  me my credit card.” Said no one, ever. If it was that easy to
-                  gain followers and influence people, everyone would be Kylie
-                  J.
+                  Developed content strategy and content to increase traction and
+                  engagement online. Entirely rewriting their site copy, and
+                  growing their social following (15% increase) to promote New
+                  West and attract visitors.{" "}
                 </p>
+              </Col>
+            </Row>
+          </Grid>
+        </section>
+
+        <section>
+          <Grid className={styles.grid} fluid>
+            <Row style={{marginTop: "2rem", marginBottom: "1rem"}}>
+              <Col sm={12}>
+                <h5>NOISE DIGITAL</h5>
+              </Col>
+            </Row>
+
+            <Row style={{marginBottom: "2rem"}}>
+              <Col sm={3}>
+                <img className={styles.img} src={k} alt="Kinder Surprise" />
+                <h6>Kinder Surprise</h6>
+              </Col>
+              <Col sm={3}>
+                <img className={styles.img} src={f} alt="Ferrero Rocher" />
+                <h6>Ferrero Rocher</h6>
+              </Col>
+              <Col sm={3}>
+                <img className={styles.img} src={n} alt="Nutella" />
+                <h6>Nutella</h6>
+              </Col>
+            </Row>
+
+            <Row>
+              <Col sm={8}>
                 <p>
-                  But it’s not, and you’re not. So who are you? And what do you
-                  want?
+                  Wrote thoughtful digital content to support the strategic
+                  marketing plans of several large international brands, including
+                  Kinder Surprise (and their national ‘Surprise Them Back’
+                  campaign), Nutella, and Ferrero Rocher.
                 </p>
-                <ul>
-                  <li>You want attention.</li>
-                  <li>You want prestige, innovation, impact.</li>
-                  <li>But most of all, you want leads that convert.</li>
-                </ul>
-                <p>
-                  I’m here to help. Your one-stop shop for content marketing and
-                  brand strategy. I’ve worked with many brands over the years,
-                  here’s just a few who’ve trusted me to energize their online
-                  presence.
-                </p>
-              </div>
-            </Col>
-          </Row>
-
-          <Row style={{marginTop: "2rem", marginBottom: "1rem"}}>
-            <Col sm={12}>
-              <h5>DISSOLVE</h5>
-            </Col>
-          </Row>
-
-          <Row style={{marginBottom: "2rem"}}>
-            <Col sm={4}>
-              <a href={GRAMMABLE_BISTRO}>
-                <img
-                  className={styles.img}
-                  src={surprised}
-                  alt="The Grammable Bistro"
-                />
-              </a>
-              <h6>The Grammable Bistro - Inedible Food, Stunning Pictures.</h6>
-            </Col>
-            <Col sm={4}>
-              <a href={VERTICAL_READY}>
-                <img
-                  className={styles.img}
-                  src={vertical}
-                  alt="Get Vertical Ready with Dissolve"
-                />
-              </a>
-              <h6>Get Vertical Ready with Dissolve</h6>
-            </Col>
-            <Col sm={4}>
-              <a href={MMUUULLTTIIPPLLY}>
-                <img
-                  className={styles.img}
-                  src={mmuuullttiipplly}
-                  alt="Achieving the MMUUULLTTIIPPLLY effect"
-                />
-              </a>
-              <h6>Achieving the MMUUULLTTIIPPLLY effect</h6>
-            </Col>
-          </Row>
-
-          <Row>
-            <Col sm={8}>
-              <p>
-                Wrote engaging, brand friendly, content and scripts for digital
-                and print ads, media outreach, and social media, increasing
-                brand awareness, click through, and sales.
-              </p>
-            </Col>
-          </Row>
-        </Grid>
-      </section>
-
-      <section className={styles.inverse}>
-        <Grid className={styles.grid} fluid>
-          <Row style={{marginTop: "2rem", marginBottom: "1rem"}}>
-            <Col sm={12}>
-              <h5>TOURISM NEW WESTMINSTER</h5>
-            </Col>
-          </Row>
-
-          <Row style={{marginBottom: "2rem"}}>
-            <Col sm={4}>
-              <a href={QUEENS_PARK}>
-                <img
-                  className={styles.img}
-                  src={park}
-                  alt="Spring Break For the Whole Family"
-                />
-              </a>
-              <h6>Spring Break For the Whole Family;</h6>
-            </Col>
-            <Col sm={4}>
-              <a href={CABARET}>
-                <img
-                  className={styles.img}
-                  src={cabaret}
-                  alt="Cabaret & Tasty Cocktails"
-                />
-              </a>
-              <h6>Cabaret & Tasty Cocktails;</h6>
-            </Col>
-            <Col sm={4}>
-              <a href={COMMUNITY}>
-                <img
-                  className={styles.img}
-                  src={community}
-                  alt="Art & Community Spaces at the Craft Cafe"
-                />
-              </a>
-              <h6>Art & Community Spaces at the Craft Cafe</h6>
-            </Col>
-          </Row>
-
-          <Row>
-            <Col sm={8}>
-              <p>
-                Developed content strategy and content to increase traction and
-                engagement online. Entirely rewriting their site copy, and
-                growing their social following (15% increase) to promote New
-                West and attract visitors.{" "}
-              </p>
-            </Col>
-          </Row>
-        </Grid>
-      </section>
-
-      <section>
-        <Grid className={styles.grid} fluid>
-          <Row style={{marginTop: "2rem", marginBottom: "1rem"}}>
-            <Col sm={12}>
-              <h5>NOISE DIGITAL</h5>
-            </Col>
-          </Row>
-
-          <Row style={{marginBottom: "2rem"}}>
-            <Col sm={3}>
-              <img className={styles.img} src={k} alt="Kinder Surprise" />
-              <h6>Kinder Surprise</h6>
-            </Col>
-            <Col sm={3}>
-              <img className={styles.img} src={f} alt="Ferrero Rocher" />
-              <h6>Ferrero Rocher</h6>
-            </Col>
-            <Col sm={3}>
-              <img className={styles.img} src={n} alt="Nutella" />
-              <h6>Nutella</h6>
-            </Col>
-          </Row>
-
-          <Row>
-            <Col sm={8}>
-              <p>
-                Wrote thoughtful digital content to support the strategic
-                marketing plans of several large international brands, including
-                Kinder Surprise (and their national ‘Surprise Them Back’
-                campaign), Nutella, and Ferrero Rocher.
-              </p>
-            </Col>
-          </Row>
-        </Grid>
-      </section>
-    </Fragment>
+              </Col>
+            </Row>
+          </Grid>
+        </section>
+      </Fragment>
   );
 };
 
